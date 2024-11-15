29af2f16c590c43df1a9a122e3f068d74a60571c
alexanderwilly <alexanderwillyj@gmail.com>
2024-11-15 14:40:09 +0800

    added faq

--- src/view/guest/GuestPage.js
+++ src/view/guest/GuestPage.js
@@ -8,6 +8,7 @@ import adopt from '../../media/adopt.png';
 import volunteer from '../../media/volunteer.png';
 import donate from '../../media/donate.png';
 import join_us  from '../../media/join-us.jpg';
+import arrow_down_icon from '../../media/arrow_down_icon.png';
 
 import DisplayAnimalsController from '../../controller/DisplayAnimalsController';
 import LoginLogoutController from '../../controller/LoginLogoutController';
@@ -47,6 +48,23 @@ const GuestPage = () =>{
             return;
         }else{
             navigate("/PetHeaven/login");
+        }
+    }
+
+    const handleDropdownFAQ = (e) => {
+        const question = e.target.closest('.question');
+        if(question){
+            const answer = question.nextElementSibling;
+            const img = question.querySelector('img');
+            if(answer.style.maxHeight === "1000px"){
+                answer.style.maxHeight = "0";
+                img.style.transform = "rotate(-90deg)";
+                answer.style.animationDelay = "0s";
+            }else{
+                answer.style.maxHeight = "1000px";
+                img.style.transform = "rotate(0deg)";
+            }
+
         }
     }
 
@@ -132,6 +150,80 @@ const GuestPage = () =>{
                 <h2>Caring for Your Pet</h2>
                 <div className = "caring-pet-video">
                     <iframe src="https://www.youtube.com/embed/x2X4vRUb_y0?si=hI94FacKG7g7FwCO" title="How to Take Care of Pets" allow="autoplay;encrypted-media;" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                </div>
+                
+            </section>
+
+            <section id = "faq" className = "faq">
+                <h2>Frequently Asked Questions</h2>
+                <div className ="questions" onClick={handleDropdownFAQ}>
+                    <div className = "question">
+                        <h3>How much is the adoption fee?</h3>
+                        <img src = {arrow_down_icon} alt = "down_icon"/>
+                    </div>
+
+                    <div className="answer">
+                        <p>
+                            <strong>The adoption fee are the following:</strong> <br/>
+                            - Puppies (below 6 months old): SG$ 250 <br/>
+                            - Adult Dogs (6 months to 7 years old): SG$ 200 <br/>
+                            - Senior Dogs (above 7 years old): SG$ 150 <br/>
+                            <br/>
+                            - Kittens (below 6 months old): SG$ 150 <br/>
+                            - Adult Cats (6 months to 7 years old): SG$ 100 <br/>
+                            - Senior Cats (above 7 years old): SG$ 50 <br/>
+                        </p>
+                    </div>
+                </div>
+
+                <div className ="questions" onClick={handleDropdownFAQ}>
+                    <div className = "question">
+                        <h3>
+                            Can I adopt if I am not a Singaporean?
+                        </h3>
+                        <img src = {arrow_down_icon} alt = "down_icon"/>
+                    </div>
+
+                    <div className="answer">
+                        <p>
+                            Yes, you can adopt even if you are not a Singaporean. 
+                            However, you must be a permanent resident or have a valid work permit in Singapore.
+                        </p>
+                    </div>
+                </div>
+
+                <div className ="questions" onClick={handleDropdownFAQ}>
+
+                    <div className = "question">
+                        <h3>
+                            Can I adopt if I live in a HDB flat?
+                        </h3>
+                        <img src = {arrow_down_icon} alt = "down_icon"/>
+                    </div>
+
+                    <div className="answer">
+                        <p>
+                            Yes, you can adopt even if you live in a HDB flat. 
+                            However, you must ensure that the breed of the dog is allowed in HDB flats.
+                        </p>
+                    </div>
+                </div>
+
+                <div className ="questions" onClick={handleDropdownFAQ}>
+                    <div className = "question">
+                        <h3>
+                            Why should I adopt a pet rather than buy?
+                        </h3>
+                        <img src = {arrow_down_icon} alt = "down_icon"/>
+                    </div>
+
+                    <div className="answer">
+                        <p>
+                            By adopting a pet, you are giving a stray or abandoned animal a second chance. 
+                            You are also helping to reduce the number of stray animals in the streets. 
+                            Moreover, you are saving a life and giving a pet a loving home.
+                        </p>
+                    </div>
                 </div>
                 
             </section>
